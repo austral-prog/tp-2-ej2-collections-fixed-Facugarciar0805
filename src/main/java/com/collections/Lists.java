cf5cf368c03ba5519fd5bbefffaf8cb64b8f9056
Facugarciar0805 <162722335+Facugarciar0805@users.noreply.github.com>
2024-08-16 21:27:53 -0300

    Update Lists.java

--- src/main/java/com/collections/Lists.java
+++ src/main/java/com/collections/Lists.java
@@ -3,5 +3,48 @@ package com.collections;
 import java.util.List;
 
 public class Lists {
+    public static Integer indexOf(String color, List<String> list) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).equals(color)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static Integer indexOfByIndex(String color, List<String> list, int index) {
+        for (int i = index; i < list.size(); i++) {
+            if (list.get(i).equals(color)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static Integer indexOfEmpty(List<String> list) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).isEmpty()) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static Integer put(String color, List<String> list) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).isEmpty()) {
+                list.set(i, color);
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static Integer remove(String color, List<String> list) {
+        int counter = 0;
+        for(int i = 0; i<list.size(); i++){
+            if(list.get(i).equals(color)){
+                list.set(i,"");
+                counter++;
+            }
+        }
+        return counter;
+    }
 
 }
